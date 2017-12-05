b0dfbd484260d7416bfa84d70c234b4710e03478
David Love <david.love@submissiontechnology.co.uk>
2017-12-05 11:04:51 +0000

    ch1 and beginning ch2 into git

---
+++ ch2-1-2.py
@@ -0,0 +1,163 @@
+import random
+from collections import namedtuple
+
+# Card = namedtuple("Card", "suit value")
+
+
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def __repr__(self):
+        return " of ".join((self.value, self.suit))
+
+
+class Deck:
+    def __init__(self):
+        self.cards = [Card(s, v) for s in ["Spades", "Clubs", "Hearts", "Diamonds"] for v in
+                      ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]]
+
+    def shuffle(self):
+        if len(self.cards) > 1:
+            random.shuffle(self.cards)
+
+    def deal(self):
+        if len(self.cards) > 1:
+            return self.cards.pop(0)
+
+
+class Hand:
+    def __init__(self, dealer=False):
+        self.dealer = dealer
+        self.cards = []
+        self.value = 0
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def calculate_value(self):
+        self.value = 0
+        has_ace = False
+        for card in self.cards:
+            if card.value.isnumeric():
+                self.value += int(card.value)
+            else:
+                if card.value == "A":
+                    has_ace = True
+                    self.value += 11
+                else:
+                    self.value += 10
+
+        if has_ace and self.value > 21:
+            self.value -= 10
+
+    def get_value(self):
+        self.calculate_value()
+        return self.value
+
+    def show(self):
+        if self.dealer:
+            return ["hidden"] + self.cards[1:]
+        else:
+            return self.cards
+
+    def display(self):
+        if self.dealer:
+            print("hidden")
+            print(self.cards[1])
+        else:
+            for card in self.cards:
+                print(card)
+            print("Value:", self.get_value())
+
+
+class Game:
+    def __init__(self):
+        playing = True
+
+        while playing:
+            self.deck = Deck()
+            self.deck.shuffle()
+
+            self.player_hand = Hand()
+            self.dealer_hand = Hand(dealer=True)
+
+            for i in range(2):
+                self.player_hand.add_card(self.deck.deal())
+                self.dealer_hand.add_card(self.deck.deal())
+
+            print("Your hand is:")
+            self.player_hand.display()
+            print()
+            print("Dealer's hand is:")
+            self.dealer_hand.display()
+
+            has_won = False
+
+            while not has_won:
+                has_won = self.check_for_blackjack()
+                if has_won:
+                    continue
+                choice = input("Please choose [Hit / Stick] ").lower()
+                while choice not in ["h", "s", "hit", "stick"]:
+                    choice = input("Please enter 'hit' or 'stick' (or H/S) ").lower()
+                if choice in ['hit', 'h']:
+                    self.player_hand.add_card(self.deck.deal())
+                    self.player_hand.display()
+                    if self.player_is_over():
+                        print("You have lost!")
+                        has_won = True
+                else:
+					player_hand_value = self.player_hand.get_value()
+					dealer_hand_value = self.dealer_hand.get_value()
+					
+                    print("Final Results")
+                    print("Your hand:", player_hand_value)
+                    print("Dealer's hand:", dealer_hand_value)
+
+                    if player_hand_value > dealer_hand_value:
+                        print("You Win!")
+                    elif player_hand_value == dealer_hand_value:
+						print("Tie!")
+                    else:
+                        print("Dealer Wins!")
+                    has_won = True
+            
+            again = input("Play Again? [Y/N] ")
+            while again.lower() not in ["y", "n"]:
+                again = input("Please enter Y or N ")
+            if again.lower() == "n":
+                print("Thanks for playing!")
+                playing = False
+            else:
+                has_won = False
+
+    def player_is_over(self):
+        return self.player_hand.get_value() > 21
+
+    def check_for_blackjack(self):
+        player = False
+        dealer = False
+        if self.player_hand.get_value() == 21:
+            player = True
+        if self.dealer_hand.get_value() == 21:
+            dealer = True
+
+        if player and dealer:
+            print("Both players have blackjack! Draw!")
+            return True
+        elif player:
+            print("You have blackjack! You win!")
+            return True
+        elif dealer:
+            print("Dealer has blackjack! Dealer wins!")
+            return True
+
+        return False
+
+
+
+
+if __name__ == "__main__":
+    g = Game()
---
+++ ch2/ch2-3.py
@@ -0,0 +1,209 @@
+import tkinter as tk
+import random
+from collections import namedtuple
+
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def __repr__(self):
+        return " of ".join((self.value, self.suit))
+
+    def get_file(self):
+        return "assets/" + self.suit + self.value + ".png"
+
+    def get_back_file(self):
+        return "assets/back.png"
+
+
+class Deck:
+    def __init__(self):
+        self.cards = [Card(s, v) for s in ["Spades", "Clubs", "Hearts", "Diamonds"] for v in
+                      ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]]
+
+    def shuffle(self):
+        if len(self.cards) > 1:
+            random.shuffle(self.cards)
+
+    def deal(self):
+        if len(self.cards) > 1:
+            return self.cards.pop(0)
+
+
+class Hand:
+    def __init__(self, dealer=False):
+        self.dealer = dealer
+        self.cards = []
+        self.value = 0
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def calculate_value(self):
+        self.value = 0
+        has_ace = False
+        for card in self.cards:
+            if card.value.isnumeric():
+                self.value += int(card.value)
+            else:
+                if card.value == "A":
+                    has_ace = True
+                    self.value += 11
+                else:
+                    self.value += 10
+
+        if has_ace and self.value > 21:
+            self.value -= 10
+
+    def get_value(self):
+        self.calculate_value()
+        return self.value
+
+    def show(self):
+        if self.dealer:
+            return ["hidden"] + self.cards[1:]
+        else:
+            return self.cards
+
+    def display(self):
+        if self.dealer:
+            print("hidden")
+            print(self.cards[1])
+        else:
+            for card in self.cards:
+                print(card)
+            print("Value:", self.get_value())
+
+class GameScreen(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Blackjack")
+        self.geometry("800x640")
+        self.resizable(False, False)
+
+        self.CARD_WIDTH_OFFSET = 300
+
+        self.PLAYER_CARD_HEIGHT = 300
+        self.DEALER_CARD_HEIGHT = 100
+
+        self.PLAYER_SCORE_TEXT_COORDS = (300, 50)
+
+        self.game_state = GameState()
+
+        self.game_screen = tk.Canvas(self, bg="white", width=800, height=500)
+
+        self.bottom_frame = tk.Frame(self, width=800, height=140, bg="red")
+        self.bottom_frame.pack_propagate(0)
+
+        self.hit_button = tk.Button(self.bottom_frame, text="Hit", width=25, command=self.hit)
+        self.stick_button = tk.Button(self.bottom_frame, text="Stick", width=25, command=self.stick)
+
+        self.hit_button.pack(side=tk.LEFT, padx=(100, 200))
+        self.stick_button.pack(side=tk.LEFT)
+
+        self.bottom_frame.pack(side=tk.BOTTOM, fill=tk.X)
+        self.game_screen.pack(side=tk.LEFT, anchor=tk.N)
+
+    def display_table(self, hide_dealer=True):
+        table_state = self.game_state.get_table_state()
+
+        player_card_images = [card.get_file() for card in table_state['player_cards']]
+        dealer_card_images = [card.get_file() for card in table_state['dealer_cards']]
+        if hide_dealer:
+            dealer_card_images[0] = card.get_back_file()
+
+        self.game_screen.create_image((250, 400), image="assets/tabletop.png")
+
+        for card_number, card_image in enumerate(self.player_card_images):
+            self.game_screen.create_image(
+                (self.CARD_WIDTH_OFFSET * card_number, self.PLAYER_CARD_HEIGHT),
+                image=card_image
+            )
+
+        for card_number, card_image in enumerate(self.dealer_card_images):
+            self.game_screen.create_image(
+                (self.CARD_WIDTH_OFFSET * card_number, self.DEALER_CARD_HEIGHT),
+                image=card_image
+            )
+
+        # create text showing hand value somewhere
+        self.game_screen.create_text(self.PLAYER_SCORE_TEXT_COORDS, text=self.game_state.player_score_as_text())
+
+        if table_state['has_winner']:
+            if table_state['has_winner'] == 'p':
+                # create you win text
+            else:
+                # create dealer wins text
+
+    def hit(self):
+        self.game_state.hit()
+        self.display_table()
+
+    def stick(self):
+        self.game_state.calculate_final_state()
+        self.display_table()
+
+
+
+
+class GameState:
+    def __init__(self)
+        self.deck = Deck()
+        self.deck.shuffle
+
+        self.player_hand = Hand()
+        self.dealer_hand = Hand(dealer=True)
+
+        for i in range(2):
+            self.player_hand.add_card(self.deck.deal())
+            self.dealer_hand.add_card(self.deck.deal())
+
+        self.has_winner = ''
+
+    def hit(self):
+        self.player_hand.add_card(self.deck.deal())
+        if self.someone_has_blackjack() == 'p':
+            self.has_winner = 'p'
+        if self.player_is_over():
+            self.has_winner = 'd'
+
+        return self.has_winner
+
+    def get_table_state(self):
+        table_state = {
+            'player_cards': self.player_hand.cards,
+            'dealer_cards': self.dealer_hand.cards,
+            'player_value': self.player_hand.get_value(),
+            'winner': self.has_winner,
+        }
+
+        return table_state
+
+    def player_score_as_text(self):
+        return "Score: " + str(self.player_hand.get_value())
+
+    def someone_has_blackjack(self):
+        player = False
+        dealer = False
+        if self.player_hand.get_value() == 21:
+            player = True
+        if self.dealer_hand.get_value() == 21:
+            dealer = True
+
+        if player and dealer:
+            return 'pb'
+        elif player:
+            return 'p'
+        elif dealer:
+            return 'd'
+
+        return False
+
+    def player_is_over(self):
+        return self.player_hand.get_value() > 21
+
+
+if __name__ == "__main__":
+    g = Game()
+    g.mainloop()
