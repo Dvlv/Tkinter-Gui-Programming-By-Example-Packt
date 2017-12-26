d88cc72f8b1759b410e6b121009b1873c03ee22a
David Love <david.love@submissiontechnology.co.uk>
2017-12-26 10:06:36 +0000

    shuffle animation

--- Ch4/ch4.py
+++ Ch4/ch4.py
@@ -1,113 +1,10 @@
 import tkinter as tk
-import os
 import random
 
-assets_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'assets/'))
+from pygame import mixer
 
-
-class Card:
-    def __init__(self, suit, value):
-        self.suit = suit
-        self.value = value
-        self.img = tk.PhotoImage(file=assets_folder + '/' + self.suit + self.value + ".png")
-
-    def __repr__(self):
-        return " of ".join((self.value, self.suit))
-
-    def get_file(self):
-        return self.img
-
-    @classmethod
-    def get_back_file(cls):
-        cls.back = tk.PhotoImage(file=assets_folder + "/back.png")
-
-        return cls.back
-
-
-class Deck:
-    def __init__(self):
-        self.cards = [Card(s, v) for s in ["Spades", "Clubs", "Hearts", "Diamonds"] for v in
-                      ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]]
-
-    def shuffle(self):
-        if len(self.cards) > 1:
-            random.shuffle(self.cards)
-
-    def deal(self):
-        if len(self.cards) > 1:
-            return self.cards.pop(0)
-
-
-class Hand:
-    def __init__(self):
-        self.cards = []
-        self.value = 0
-
-    def add_card(self, card):
-        self.cards.append(card)
-
-    def calculate_value(self):
-        self.value = 0
-        has_ace = False
-        for card in self.cards:
-            if card.value.isnumeric():
-                self.value += int(card.value)
-            else:
-                if card.value == "A":
-                    has_ace = True
-                    self.value += 11
-                else:
-                    self.value += 10
-
-        if has_ace and self.value > 21:
-            self.value -= 10
-
-    def get_value(self):
-        self.calculate_value()
-        return self.value
-
-
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.money = 50
-        self.hand = Hand()
-
-    def add_winnings(self, winnings):
-        self.money += winnings
-
-    def place_bet(self, amount):
-        self.money -= amount
-
-    def can_place_bet(self, amount):
-        return self.money >= amount
-
-    def receive_card(self, card):
-        self.hand.add_card(card)
-
-    def empty_hand(self):
-        self.hand.cards = []
-
-    @property
-    def score(self):
-        return self.hand.get_value()
-
-    @property
-    def is_over(self):
-        return self.hand.get_value() > 21
-
-    @property
-    def has_blackjack(self):
-        return self.hand.get_value() == 21
-
-    @property
-    def cards(self):
-        return [card for card in self.hand.cards]
-
-
-class Dealer(Player):
-    def __init__(self):
-        super().__init__("Dealer")
+from casino import Card, Deck, Player, Dealer, assets_folder
+from casino_sounds import SoundBoard
 
 
 class GameScreen(tk.Tk):
@@ -131,6 +28,8 @@ class GameScreen(tk.Tk):
         self.game_state = GameState("Player")
 
         self.game_screen = tk.Canvas(self, bg="white", width=800, height=500)
+        self.tabletop_image = tk.PhotoImage(file=assets_folder + "/tabletop.png")
+        self.card_back_image = Card.get_back_file()
 
         self.bottom_frame = tk.Frame(self, width=800, height=140, bg="red")
         self.bottom_frame.pack_propagate(0)
@@ -147,7 +46,33 @@ class GameScreen(tk.Tk):
         self.bottom_frame.pack(side=tk.BOTTOM, fill=tk.X)
         self.game_screen.pack(side=tk.LEFT, anchor=tk.N)
 
-        self.display_table()
+        #self.display_table()
+        self.display_opening_animation()
+
+    def display_opening_animation(self):
+
+        self.game_screen.create_image((400, 250), image=self.tabletop_image)
+
+        self.card_back_1 = self.game_screen.create_image((700, 100), image=self.card_back_image)
+        self.card_back_2 = self.game_screen.create_image((720, 100), image=self.card_back_image)
+
+        self.back_1_movement = ([10] * 6 + [-10] * 6) * 7
+        self.back_2_movement = ([-10] * 6 + [10] * 6) * 7
+
+        self.frame = 0
+
+        self.play_card_animation()
+
+    def play_card_animation(self):
+        if self.frame < len(self.back_1_movement):
+            self.game_screen.move(self.card_back_1, self.back_1_movement[self.frame], 0)
+            self.game_screen.move(self.card_back_2, self.back_2_movement[self.frame], 0)
+            self.game_screen.update()
+            self.frame += 1
+    
+            self.after(33, self.play_card_animation)
+
+
 
     def display_table(self, hide_dealer=True, table_state=None):
         if not table_state:
@@ -159,7 +84,6 @@ class GameScreen(tk.Tk):
             dealer_card_images[0] = Card.get_back_file()
 
         self.game_screen.delete("all")
-        self.tabletop_image = tk.PhotoImage(file=assets_folder + "/tabletop.png")
 
         self.game_screen.create_image((400, 250), image=self.tabletop_image)
 
@@ -227,8 +151,11 @@ class GameState:
         self.current_round = 1
         self.pot = 0
 
+        self.soundboard = SoundBoard()
+
         self.deck = Deck()
         self.deck.shuffle()
+        self.soundboard.shuffle_sound.play()
 
         self.player = Player(player_name)
         self.dealer = Dealer()
