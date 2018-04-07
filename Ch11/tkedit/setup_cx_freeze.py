d5f02e506b88eb3105c85437005057c0a98a39bf
David Love <Dvlv@users.noreply.github.com>
2018-04-07 12:41:20 +0100

    Update setup_cx_freeze.py

--- Ch11/tkedit/setup_cx_freeze.py
+++ Ch11/tkedit/setup_cx_freeze.py
@@ -1,11 +1,22 @@
 from cx_Freeze import setup, Executable
 
-# Dependencies are automatically detected, but it might need
-# fine tuning.
-buildOptions = dict(packages = [], excludes = [])
-
 import sys
 base = 'Win32GUI' if sys.platform=='win32' else None
+
+import os
+import os.path
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
+options = {
+    'build_exe': {
+        'include_files':[
+            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
+         ],
+    },
+}
 
 executables = [
     Executable('tkedit.py', base=base)
@@ -14,5 +25,5 @@ executables = [
 setup(name='tkedit',
       version = '1.0',
       description = 'A tkinter text editor',
-      options = dict(build_exe = buildOptions),
+      options = options,
       executables = executables)
